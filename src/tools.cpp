01cbf416657528638ecfb2a8f03a103699bb0069
David Chrzanowski <chrzanowski.david@gmail.com>
2017-10-16 20:42:17 -0400

    Introduce a check for x and y pos values near 0 and decreasing panic threshold for c1 param

--- src/tools.cpp
+++ src/tools.cpp
@@ -54,13 +54,24 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 	float vx = x_state(2);
 	float vy = x_state(3);
 
+	//This is new . . . hopefully combats craziness when xpos is near 0
+	if (fabs(px) < .0001){
+		px = .0001;
+	}
+
+	if (fabs(py) < .0001){
+		py = .0001;
+	}
+
+
 	//pre-compute a set of terms to avoid repeated calculation
 	float c1 = px*px+py*py;
 	float c2 = sqrt(c1);
 	float c3 = (c1*c2);
 
 	//check division by zero
-	if(fabs(c1) < 0.0001){
+	//also this is new, using even smaller number threshold
+	if(fabs(c1) < 0.0000001){
 		cout << "CalculateJacobian () - Error - Division by Zero" << endl;
 		return Hj;
 	}
